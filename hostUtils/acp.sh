ae564172b741d690bda17f545109b0f4f145a1a1
Alexander Zinin <spore_09@mail.ru>
2019-07-07 16:21:50 +0900

    refact: create shared function for host utils

---
+++ hostUtils/acp.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ACP_API_VERSION="ssc-v1"
+ACP_DELIMITER=" ; "
+
+function join_by {
+  local d=$1
+  shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"
+}
+
+sendAcpData() {
+  echo -ne '\x1B\x9F' # ESC ACP
+  echo -ne `join_by "$ACP_DELIMITER" "$ACP_API_VERSION" $*`
+  echo -ne '\x9C' # ST
+}
